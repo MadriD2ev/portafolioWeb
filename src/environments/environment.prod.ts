67631ebfd0fe162bb4b597463702c4a47d700b5e
Madrid GH <m.h.gutierrez@evomatik.com>
2020-10-12 21:49:05 -0500

    implementacion de hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCD4T7H2QTtEmXyu6XZAkVcMba2DrM1rTY",
+    authDomain: "portafoliowebmadrid.firebaseapp.com",
+    databaseURL: "https://portafoliowebmadrid.firebaseio.com",
+    projectId: "portafoliowebmadrid",
+    storageBucket: "portafoliowebmadrid.appspot.com",
+    messagingSenderId: "232602549003",
+    appId: "1:232602549003:web:2c5e3e3bfed87a224cc00e"
+  }
 };
+
+
